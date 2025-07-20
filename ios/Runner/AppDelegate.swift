45c30df94fb2a141e40e58d930fe8e2021b8c0bb
Abdullahi ismail <hena@Abdullahis-Mac.local>
2025-07-20 06:12:56 -0700

    update for flutter plugin in xcode

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,15 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+      let messenger = self.window?.rootViewController as! FlutterBinaryMessenger
+
+          // Register the LidarScanViewFactory with the Flutter engine.
+          // The string "com.yourcompany.bodyscan/LidarScanView" is the unique viewType identifier.
+          // This MUST match the viewType you will use in your Dart code.
+          self.registrar(forPlugin: "LidarScanViewFactory")!.register(
+              LidarScanViewFactory(messenger: messenger),
+              withId: "com.yourcompany.bodyscan/LidarScanView"
+          )
+      return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
